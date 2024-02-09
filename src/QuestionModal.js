1a1b6ecfab30870deec21f19d6174758f68623f3
şöhrett <sohretturaman@gmail.com>
2024-02-09 22:55:39 +0300

    taken api response has converted to formatted data

---
+++ src/QuestionModal.js
@@ -0,0 +1,11 @@
+/** @format */
+
+ class QuestionModal {
+  constructor(id, category, question, options, answer) {
+    (this.id = id),
+      (this.category = category),
+      (this.question = question),
+      (this.options = options),
+      (this.answer = answer);
+  }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,69 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Question from "../components/Question";
 import styles from "./styles.module.css";
 import CheckButton from "../components/CheckButton";
+import { nanoid } from "nanoid";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const shuffleArray = (arr, item) => {
+    let n = arr.length;
+    let randomIndex = Math.floor(Math.random() * (n + 1)); // generate random index including the end of the array
+    arr.splice(randomIndex, 0, item); // insert 'x' at random index
+    for (let i = n - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // generate random index to swap with
+      [arr[i], arr[j]] = [arr[j], arr[i]]; // swap elements at indices i and j
+    }
+    return arr;
+  };
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then((jsonData) => jsonData.json())
+      .then((result) => {
+        const data = result.results;
+        const newData = [];
+
+        data?.map((item) => {
+          let randomId = nanoid();
+          let options = shuffleArray(
+            item.incorrect_answers,
+            item.correct_answer
+          );
+          newData.push({
+            id: randomId,
+            category: item.category,
+            question: item.question,
+            answer: item.correct_answer,
+            options: options,
+          });
+        });
+ setData(newData);
+      
+      });
+  }, []);
+
+
+
+  const questionsLlist = data?.map((item) => {
+    return (
+      <Question
+        key={item.id}
+      {...item}
+      />
+    );
+  })
   return (
-    <main className={styles.hContainer}>
-      <div>
-        <Question question={"1. How would you say goody in Spanish ?"} />
-        <Question
-          question={
-            "2. Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?  riots breaking in stores"
-          }
-        />
-        <Question
-          question={"3. In which country was the caesar salad invented?"}
-        />
-        <Question question={"4. How Many Hearts Does An Octopus Have?"} />
-        <Question question={"5. How Many Hearts Does An Octopus Have?"} />
+    <div className={styles.hContainer}>
+      <main>
+       {questionsLlist}
+      </main>
+      <div className={styles.checkBWrapper}>
+        <CheckButton text="Check Answers" />
       </div>
-      <div  className={styles.checkBWrapper}>
-        <CheckButton text="Check Answers"/>
-      </div>
-    </main>
+    </div>
   );
 };
 
